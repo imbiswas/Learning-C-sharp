899ea6f6df3e4c39a57f06d438fc91a93fe994da
imbiswas <biswas.shre@hotmail.com>
2021-06-07 11:08:58 +0200

    camera 2

---
+++ camera 2/camera 2/NewWinFormCsharpWebCam/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace NewWinFormCsharpWebCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntVideoSource = new System.Windows.Forms.Button();
+            this.bntVideoFormat = new System.Windows.Forms.Button();
+            this.bntSave = new System.Windows.Forms.Button();
+            this.bntCapture = new System.Windows.Forms.Button();
+            this.bntContinue = new System.Windows.Forms.Button();
+            this.bntStop = new System.Windows.Forms.Button();
+            this.bntStart = new System.Windows.Forms.Button();
+            this.imgCapture = new System.Windows.Forms.PictureBox();
+            this.imgVideo = new System.Windows.Forms.PictureBox();
+            this.exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCapture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bntVideoSource
+            // 
+            this.bntVideoSource.FlatAppearance.BorderSize = 0;
+            this.bntVideoSource.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bntVideoSource.Location = new System.Drawing.Point(424, 133);
+            this.bntVideoSource.Name = "bntVideoSource";
+            this.bntVideoSource.Size = new System.Drawing.Size(147, 23);
+            this.bntVideoSource.TabIndex = 17;
+            this.bntVideoSource.Text = "Video Source";
+            this.bntVideoSource.UseVisualStyleBackColor = true;
+            this.bntVideoSource.Click += new System.EventHandler(this.bntVideoSource_Click);
+            // 
+            // bntVideoFormat
+            // 
+            this.bntVideoFormat.FlatAppearance.BorderSize = 0;
+            this.bntVideoFormat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bntVideoFormat.Location = new System.Drawing.Point(424, 104);
+            this.bntVideoFormat.Name = "bntVideoFormat";
+            this.bntVideoFormat.Size = new System.Drawing.Size(147, 23);
+            this.bntVideoFormat.TabIndex = 16;
+            this.bntVideoFormat.Text = "Video Format";
+            this.bntVideoFormat.UseVisualStyleBackColor = true;
+            this.bntVideoFormat.Click += new System.EventHandler(this.bntVideoFormat_Click);
+            // 
+            // bntSave
+            // 
+            this.bntSave.FlatAppearance.BorderSize = 0;
+            this.bntSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bntSave.Location = new System.Drawing.Point(326, 220);
+            this.bntSave.Name = "bntSave";
+            this.bntSave.Size = new System.Drawing.Size(79, 23);
+            this.bntSave.TabIndex = 15;
+            this.bntSave.Text = "Save Image";
+            this.bntSave.UseVisualStyleBackColor = true;
+            this.bntSave.Click += new System.EventHandler(this.bntSave_Click);
+            // 
+            // bntCapture
+            // 
+            this.bntCapture.FlatAppearance.BorderSize = 0;
+            this.bntCapture.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bntCapture.Location = new System.Drawing.Point(242, 220);
+            this.bntCapture.Name = "bntCapture";
+            this.bntCapture.Size = new System.Drawing.Size(85, 23);
+            this.bntCapture.TabIndex = 14;
+            this.bntCapture.Text = "Capture Image";
+            this.bntCapture.UseVisualStyleBackColor = true;
+            this.bntCapture.Click += new System.EventHandler(this.bntCapture_Click);
+            // 
+            // bntContinue
+            // 
+            this.bntContinue.FlatAppearance.BorderSize = 0;
+            this.bntContinue.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bntContinue.Location = new System.Drawing.Point(156, 220);
+            this.bntContinue.Name = "bntContinue";
+            this.bntContinue.Size = new System.Drawing.Size(61, 23);
+            this.bntContinue.TabIndex = 13;
+            this.bntContinue.Text = "Continue";
+            this.bntContinue.UseVisualStyleBackColor = true;
+            this.bntContinue.Click += new System.EventHandler(this.bntContinue_Click);
+            // 
+            // bntStop
+            // 
+            this.bntStop.FlatAppearance.BorderSize = 0;
+            this.bntStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bntStop.Location = new System.Drawing.Point(101, 220);
+            this.bntStop.Name = "bntStop";
+            this.bntStop.Size = new System.Drawing.Size(49, 23);
+            this.bntStop.TabIndex = 12;
+            this.bntStop.Text = "Stop";
+            this.bntStop.UseVisualStyleBackColor = true;
+            this.bntStop.Click += new System.EventHandler(this.bntStop_Click);
+            // 
+            // bntStart
+            // 
+            this.bntStart.FlatAppearance.BorderSize = 0;
+            this.bntStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bntStart.Location = new System.Drawing.Point(54, 220);
+            this.bntStart.Name = "bntStart";
+            this.bntStart.Size = new System.Drawing.Size(41, 23);
+            this.bntStart.TabIndex = 11;
+            this.bntStart.Text = "Start";
+            this.bntStart.UseVisualStyleBackColor = true;
+            this.bntStart.Click += new System.EventHandler(this.bntStart_Click);
+            // 
+            // imgCapture
+            // 
+            this.imgCapture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.imgCapture.Location = new System.Drawing.Point(242, 45);
+            this.imgCapture.Name = "imgCapture";
+            this.imgCapture.Size = new System.Drawing.Size(163, 160);
+            this.imgCapture.TabIndex = 10;
+            this.imgCapture.TabStop = false;
+            // 
+            // imgVideo
+            // 
+            this.imgVideo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.imgVideo.Location = new System.Drawing.Point(54, 45);
+            this.imgVideo.Name = "imgVideo";
+            this.imgVideo.Size = new System.Drawing.Size(163, 160);
+            this.imgVideo.TabIndex = 9;
+            this.imgVideo.TabStop = false;
+            // 
+            // exit
+            // 
+            this.exit.FlatAppearance.BorderSize = 0;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exit.ForeColor = System.Drawing.Color.Red;
+            this.exit.Location = new System.Drawing.Point(565, 11);
+            this.exit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(24, 19);
+            this.exit.TabIndex = 18;
+            this.exit.Text = "X";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.bntVideoSource);
+            this.Controls.Add(this.bntVideoFormat);
+            this.Controls.Add(this.bntSave);
+            this.Controls.Add(this.bntCapture);
+            this.Controls.Add(this.bntContinue);
+            this.Controls.Add(this.bntStop);
+            this.Controls.Add(this.bntStart);
+            this.Controls.Add(this.imgCapture);
+            this.Controls.Add(this.imgVideo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NEW CAMERA PROJET VS2017";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgCapture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntVideoSource;
+        private System.Windows.Forms.Button bntVideoFormat;
+        private System.Windows.Forms.Button bntSave;
+        private System.Windows.Forms.Button bntCapture;
+        private System.Windows.Forms.Button bntContinue;
+        private System.Windows.Forms.Button bntStop;
+        private System.Windows.Forms.Button bntStart;
+        private System.Windows.Forms.PictureBox imgCapture;
+        private System.Windows.Forms.PictureBox imgVideo;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
